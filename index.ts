08ff273432502fe6d4c10f1a58ff6729c0ec32b7
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2024-02-07 16:38:20 -0300

    add new ejer:

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { dnaStrand } from "./Ejer/ejer1";
 import { XO } from './Ejer/ejer2'
+import { solution } from './Ejer/ejer3'
 
-console.log(XO("dsafas"));
+console.log(solution("yoSoyCamelCase"));
 
